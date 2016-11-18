2abe9ab446583d3fc3408db2ab4a55462f70f944
englandt <tengland@TRISTARGROUP.NET>
2016-11-18 14:30:56 -0800

    test files
    mocha + chai

---
+++ test/test-server.js
@@ -0,0 +1,26 @@
+var chai = require('chai');
+var expect = chai.expect;
+var should = chai.should();
+var chaiHTTP = require('chai-http');
+var server = require('../server.js');
+
+chai.use(chaiHTTP);
+
+describe('GET home', function() {
+    it('should render home page', function(done) {
+	chai.request(server.app)
+	    .get('/home')
+	    .end(function(err, res) {
+		    expect(200, done());
+	    });
+    });
+});
+
+describe('get DB Conn', function() {
+   it('should return database connection object', function(done) {
+       server.getConn()
+	   .should.be.an('object');
+       done();
+   });
+});
+
